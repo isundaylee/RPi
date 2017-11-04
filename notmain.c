4324a039ee5cf5694df7c8ae3c891a10e3466f3d
Jiahao Li <isundaylee.reg@gmail.com>
2017-11-04 15:53:09 -0400

    Simple example

--- notmain.c
+++ notmain.c
@@ -2,15 +2,27 @@
 #include "lib/gpio.h"
 #include "lib/timer.h"
 
+#define PIN_LED 47
+
 int notmain(void) {
   aux_mu_init();
 
   aux_mu_send_string("Hello, world!\r\n");
   aux_mu_flush();
 
+  gpio_configure(PIN_LED, GPIO_OUTPUT);
+
   while (1) {
-      aux_mu_send(aux_mu_receive());
-      aux_mu_flush();
+    char c = aux_mu_receive();
+
+    switch (c) {
+    case '0':
+      gpio_set(PIN_LED);
+      break;
+    case '1':
+      gpio_clear(PIN_LED);
+      break;
+    }
   }
 
   return 0;
