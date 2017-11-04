3b12d570058f522ea576d9617f4822de9fa794d9
Jiahao Li <isundaylee.reg@gmail.com>
2017-11-04 01:11:52 -0400

    Timer WIP

--- lib/timer.c
+++ lib/timer.c
@@ -1,3 +1,21 @@
-void timer_init() {
+#include "lib/timer.h"
 
+typedef struct {
+  uint32_t cs;
+  uint32_t clo;
+  uint32_t chi;
+  uint32_t c[4];
+} Timer;
+
+#define TIMER_BASE (PERIPHERAL_BASE + 0x00003000)
+
+volatile Timer *timer = (Timer *)TIMER_BASE;
+
+void timer_init() {}
+
+uint32_t timer_get() {
+  dmb();
+  return timer->clo;
 }
+
+void timer_set(int timer, int us, int type) {}
--- lib/timer.h
+++ lib/timer.h
@@ -2,6 +2,13 @@
 
 #include "lib/common.h"
 
+#define TIMER_1 1
+#define TIMER_1 3
+
+#define TIMER_ONCE 0
+#define TIMER_PERIODIC 1
+
 #define TIMER_BASE (PERIPHERAL_BASE + 0x00003000)
 
 void timer_init();
+void timer_set(int timer, int us, int type);
